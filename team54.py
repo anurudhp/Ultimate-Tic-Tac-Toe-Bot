abd43c7819fae227b7abf399117ed1b5269b4d2c
Arjun P <arjunpitchanathan@gmail.com>
2017-02-23 03:39:13 +0530

    tried some experiments

--- team54.py
+++ team54.py
@@ -10,11 +10,11 @@ SCORE_TRIPLE = 10**0
 SCORE_GAME_CELL   = 10**6
 SCORE_GAME_PAIR   = 10**3
 SCORE_GAME_TRIPLE = 10**0
-WEIGHT_ATTACK = SCORE_GAME_CELL
-WEIGHT_GAME = 1
+WEIGHT_ATTACK = 1
+WEIGHT_GAME = 2
 
 class Player54():
-    def __init__(self, max_depth = 4, max_breadth = 16 ** 10, must_prune = True):
+    def __init__(self, max_depth = 3, max_breadth = 16 ** 10, must_prune = True):
         self.max_depth = max_depth
         self.max_breadth = max_breadth
         self.must_prune = must_prune
